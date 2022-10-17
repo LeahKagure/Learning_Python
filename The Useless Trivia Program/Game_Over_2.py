34cc9d2ad423c1d0ca62043770efe8623553340a
LeahKagure <leah.kagure98@gmail.com>
2022-10-17 10:33:02 +0300

    Types, Variables, and Simple I/O

---
+++ The Useless Trivia Program/Game_Over_2.py
@@ -0,0 +1,29 @@
+# Game Over - Version 2
+# Demonstrates the use of quotes in strings
+print("Program 'Game Over' 2.0")
+print("Same", "message", "as before")
+print("Just",
+      "a bit",
+      "bigger")
+print("Here", end=" ")
+print("it is...")
+print(
+ """
+Chapter 2 • Types, Variables, and Simple I/O: The Useless Trivia Program 17
+ ____         ___       ___  ___   _____
+/ ___|       /   |     /   |/  |  | ___|
+| |         / /| |    / /|  /| |  | |__
+| |  _     / ___ |   / / |_/ | |  |  __|
+| |_| |   / /  | |  / /      | |  | |___
+\____ /  /_/   |_| /_/       |_|  |_____|
+
+ _____  __     __   _____  _____
+/  _  \ | |   / /  | ____| |  _ \
+| | | | | |  / /   | |___  | |_| |
+| | | | | | / /    |  ___| |  _  /
+| |_| | | |/ /     | |___  | | \ \
+\_____/ |___/      |_____| |_|  \_\
+
+"""
+ )
+input("\n\nPress the enter key to exit.")
