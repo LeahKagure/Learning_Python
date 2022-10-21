db0993c55bcbcf2a31440aa3ae65a1da02f2107c
LeahKagure <leah.kagure98@gmail.com>
2022-10-21 15:42:17 +0300

    GUI Development

---
+++ The Mad Lib Program/Simple_GUI.py
@@ -0,0 +1,14 @@
+# Simple GUI
+# Demonstrates creating a window
+
+from tkinter import *
+
+# Create the root window
+root = Tk()
+
+# Modify the window
+root.title("Simple GUI")
+root.geometry("200x100")
+
+# Kick off the window's event loop
+root.mainloop()
